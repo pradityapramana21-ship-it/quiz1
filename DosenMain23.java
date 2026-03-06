b90c13e0cd423cac607e2f261051faa93063e2cb
radit <pradityapramana21@gmail.com>
2026-03-06 09:10:19 +0700

    3

---
+++ DosenMain23.java
@@ -0,0 +1,8 @@
+public class DosenMain23 {
+public static void main(String[] args) {
+    Dosen23 d1 = new Dosen23("88878878", "Wahyu Pratama", 2015, "S2", "Teknik Informatika");
+    d1.tampilkanInfo();
+    d1.ubahProdi("Teknik Mesin");
+    d1.ubahPendidikan("S3");
+System.out.println("Setelah diubah:");
+    d1.tampilkanInfo(); } }
